745991da192c88bd5fabf79baed6965ee50f8eff
anang0g0 <test@chagai>
2022-05-20 12:11:19 +0900

    triming

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 // y: シード値（32ビット整数）
 extern crate rand;
 use rand::Rng;
-use std::process::exit;
+//use std::process::exit;
 
 
 /*
@@ -13,20 +13,14 @@ fn random_shuffule(mut array: [i32;8192], size:i32)-> [i32;8192]
     let mut _i:usize;
     let mut a:usize;
     let mut b:usize;
-     let mut temp:i32;
-    let mut it:i32;
 
 
-    print!("{}",array[0]);
     for _i in (1..size).rev()
     {
         a = (_i - 1) as usize;
         b = rand::thread_rng().gen_range(1, 8192)% _i as usize;
         // ソートするキーの型
-        temp=array[a];
-        array[a]=array[b];
-        array[b]=temp;
-        //(array[a] , array[b] ) = ( array[b], array[a] )
+        (array[a] , array[b] ) = ( array[b], array[a] )
     }
     array
 }
@@ -37,7 +31,7 @@ fn main() {
 
     for _i in 1..8192
     {
-    a[_i]= rand::thread_rng().gen_range(1, 8192);
+    a[_i]= _i as i32; 
     }
     //exit(1);
     a=random_shuffule(a,8192);
