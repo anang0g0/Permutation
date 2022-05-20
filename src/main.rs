3bcf46f8461e8c6f8e00002f24c448d1b767eccd
anang0g0 <test@chagai>
2022-05-20 10:31:38 +0900

    fail

--- src/main.rs
+++ src/main.rs
@@ -2,55 +2,38 @@
 extern crate rand;
 use rand::Rng;
 
-#[derive(Debug)]
-struct srt {
-    rand: i32,
-    ind: i32,
-}
 
-
-// ソートするキーの型
-fn SWAP(a:usize,  b:usize)-> crate::swp
-    {
-        let mut work;
-        srp o;
-
-        work = a; 
-        a = b;           
-        b = work;
-        o.rand=a;
-        o.ind=b;
-    o
-    }
 
 /*
     Fisher-Yates shuffle による方法
     配列の要素をランダムシャッフルする
 */
-fn random_shuffule(array:[i32;8192], size:i32)
+fn random_shuffule(array:[i32;8192], size:i32)->&[i32;8192]
 {
     let mut i:usize;
-    let mut a:usize;
+    let a:usize;
     let mut b:usize;
 
     for i in (1..size).rev()
     {
         a = (i - 1) as usize;
         b = rand::thread_rng().gen_range(1, i) as usize;
-        SWAP(array[a] as usize, array[b] as usize);
+        // ソートするキーの型
+        //(array[a] , array[b] ) = ( array[b], array[a] )
     }
+    &array
 }
 
 fn main() {
-    let a:[i32;8192];
+    let mut a:[i32;8192];
     let mut i:i32;
 
     for i in 1..8192
     {
-    a[i]= rand::thread_rng().gen_range(1, 8192);
+    a[i]= rand::thread_rng().gen_range(1, 8192) as i32;
     }
 
-    random_shuffule(a,8192);
+    //random_shuffule(a,8192);
     
     for i in 1..8192{
         print!("{},",a[i]);
